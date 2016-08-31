18052f1bc2106dc9d1daa69a019a477e14089650
masterUNG <phrombutr@gmail.com>
2016-08-31 10:55:05 +0700

    post data Finish

--- app/src/main/java/appewtc/masterung/rdrun/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/rdrun/SignUpActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -161,6 +162,7 @@ public class SignUpActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Response response) throws IOException {
+                Log.d("31AugV1", "Result ==> " + response.body().string());
                 finish();
             }
         });
