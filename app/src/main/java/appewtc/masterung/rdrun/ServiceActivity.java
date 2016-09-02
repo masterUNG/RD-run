308cfe520561b1a8b2031d0c0a865ba095ef3707
masterUNG <phrombutr@gmail.com>
2016-09-02 13:35:06 +0700

    Marker Create OK

--- app/src/main/java/appewtc/masterung/rdrun/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/rdrun/ServiceActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
@@ -25,6 +26,9 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -88,6 +92,10 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         private Context context;
         private GoogleMap googleMap;
         private static final String urlJSON = "http://swiftcodingthai.com/rd/get_user_master.php";
+        private String[] nameStrings, surnameStrings;
+        private int[] avataInts;
+        private double[] latDoubles, lngDoubles;
+
 
         public SynAllUser(Context context, GoogleMap googleMap) {
             this.context = context;
@@ -117,6 +125,36 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
             super.onPostExecute(s);
 
             Log.d("2SepV2", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                nameStrings = new String[jsonArray.length()];
+                surnameStrings = new String[jsonArray.length()];
+                avataInts = new int[jsonArray.length()];
+                latDoubles = new double[jsonArray.length()];
+                lngDoubles = new double[jsonArray.length()];
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    nameStrings[i] = jsonObject.getString("Name");
+                    surnameStrings[i] = jsonObject.getString("Surname");
+                    avataInts[i] = Integer.parseInt(jsonObject.getString("Avata"));
+                    latDoubles[i] = Double.parseDouble(jsonObject.getString("Lat"));
+                    lngDoubles[i] = Double.parseDouble(jsonObject.getString("Lng"));
+
+                    //Create Marker
+                    googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(latDoubles[i], lngDoubles[i])));
+
+                }   // for
+
+            } catch (Exception e) {
+                Log.d("2SepV3", "e onPost ==> " + e.toString());
+            }
 
 
         }   // onPost
@@ -234,6 +272,8 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
 
     private void createMarker() {
 
+        //Clear Marker
+        mMap.clear();
 
         SynAllUser synAllUser = new SynAllUser(this, mMap);
         synAllUser.execute();
